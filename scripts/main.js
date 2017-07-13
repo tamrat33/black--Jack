8720da17cba72ad13c71d70df155358a6a3a29db
tamrat teare <tamrat33@gmail.com>
2017-07-13 15:44:37 -0400

    7 card passed the test

--- scripts/main.js
+++ scripts/main.js
@@ -5,15 +5,35 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
 
 
-function handValue (hand) {
+  function handValue (cards) {
+  let totalValue= 0;
+  for (let i = 0; i <cards.length ; i++) {
+   if (cards[i] === "K" || cards[i] === "J" || cards [i] === "Q"){
+     cards[i]= 10;
+   } else if (cards[i] === "A") {
+     cards[i] =11;
+   }
+
+    totalValue = parseInt(cards[i]) + totalValue
+  }
+
+  return totalValue;
 
 
-  return;
+
+
+//convert  string value  into number
+// loop over all cards and convert to totalValue
+// loop over allcards and convert to
+ totalValue
+// Add numerical vallue to totalvalue
+
+
 }
 
 
--- scripts/tests.js
+++ scripts/tests.js
@@ -10,7 +10,9 @@
 
   // Blackjack Hand Tests
   // Hand, Value
+  assert(["2"], 2);
   assert(["2", "2", "8"], 12);
+
   assert(["2", "2", "K"], 14);
   assert(["2", "Q"], 12);
   assert(["7", "J"], 17);
